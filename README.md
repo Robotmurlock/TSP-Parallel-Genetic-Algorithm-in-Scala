1137fe5c7e530c12f2ce36beae22f1ee50f656dc
Robotmurlock <momir.adzemovic@gmail.com>
2020-03-13 11:56:22 +0100

    Implemented problem input interface

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TSP-Parallel-Genetic-Algorithm-in-Scala
 Implementation of Parallel Genetic Algorithm in Scala for solving Travelling Salesman Problem.
+
+# How to use:
+`position in sources`
+`sbt`
+`compile`
+`run 1.txt`
--- source/src/main/scala/main.scala
+++ source/src/main/scala/main.scala
@@ -1,5 +1,31 @@
+import scala.io.Source
+import java.util.Scanner
+import java.io.File
+
+
 object HelloSbt {
     def main(args: Array[String]) = {
-        println("Hello from main!")
+        val prefix = "src/main/tsp_examples/"
+        val filename = prefix + args(0)
+
+        val sc = new Scanner(new File(filename))
+        
+        val n = sc.nextInt()
+        val matrix = Array.ofDim[Int](n,n)
+
+        for(i <- 0 to n-1) {
+            for(j <- 0 to n-1) {
+                matrix(i)(j) = sc.nextInt()
+            }
+        }
+
+        // Testing file input
+        for(i <- 0 to n-1) {
+            for(j <- 0 to n-1) {
+                print(matrix(i)(j))
+                print(' ')
+            }
+            println()
+        }
     }
 }
