43719efdf36eea6d790f6120770c4170fe60cf90
Robotmurlock <momir.adzemovic@gmail.com>
2020-04-19 18:42:22 +0200

    Refactored code

--- genetic_algorithm/src/main/scala/main.scala
+++ genetic_algorithm/src/main/scala/main.scala
@@ -1,54 +1,7 @@
 import scala.io.Source
-import java.util.Scanner
-import java.io.File
 import genetic_algorithm.GA
-import scala.io.Codec
-import java.nio.charset.CodingErrorAction
-
-object CSVReader{
-    val delimiter = ";"
-
-    def read_from_csv(file: String) : Array[Array[Double]] = {
-        implicit val codec = Codec("UTF-8")
-        codec.onMalformedInput(CodingErrorAction.REPLACE)
-        codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
-        val bufferedSource = io.Source.fromFile(file)
-        var cost_matrix: Array[Array[Double]] = null
-        var j: Int = 0
-        var k: Int = 0
-        var first_line: Boolean = true
-        for (line <- bufferedSource.getLines()) {
-            if(!first_line) {
-                var cols = line.split(delimiter).map(_.trim)
-                // skips first columns
-                cols = cols.slice(1, cols.size)
-
-                // memory optimization
-                if(cost_matrix == null) {
-                    cost_matrix = Array.ofDim[Double](cols.size, cols.size)
-                }
-
-                for (cell <- cols) {
-                    cost_matrix(j)(k) = cell.toDouble
-                    k += 1
-                }
-                k = 0
-                j += 1
-            }
-            else {
-                // skips header
-                first_line = false
-            }
-        }
-        for(i <- 0 until j) {
-            for(k<-0 until j){
-                print(cost_matrix(i)(k) + " ")
-            }
-            println("")
-        }
-        return(cost_matrix)
-    }
-}
+import reader.CSVReader
+import reader.TXTReader
 
 object Main {
     def main(args: Array[String]) : Unit = {
@@ -57,22 +10,14 @@ object Main {
         var log: Boolean = false
 
         var cost_matrix: Array[Array[Double]] = null
-        if(args(1) == "-as-csv") {
-            cost_matrix = CSVReader.read_from_csv(filename)
+        if(args.size > 1 &&  args(1) == "-as-csv") {
+            cost_matrix = CSVReader.read(filename)
         }
         else {
-            val sc = new Scanner(new File(filename))
-            val dimension = sc.nextInt()
-            cost_matrix = Array.ofDim[Double](dimension, dimension)
-
-            for(i <- 0 to dimension-1) {
-                for(j <- 0 to dimension-1) {
-                    cost_matrix(i)(j) = sc.nextDouble()
-                }
-            }
+            cost_matrix = TXTReader.read(filename)
         }
 
-        val ga = new GA(cost_matrix, 100, 100, 1, 0.2, 0.01, 10, log)
+        val ga = new GA(cost_matrix, 1000, 100, 1, 0.2, 0.01, 10, log)
         ga.run()
     }
 }
---
+++ genetic_algorithm/src/main/scala/reader.scala
@@ -0,0 +1,61 @@
+import scala.io.Codec
+import java.nio.charset.CodingErrorAction
+import scala.io.Source
+import java.util.Scanner
+import java.io.File
+
+package reader {
+    object CSVReader{
+        val delimiter = ";"
+
+        def read(file: String) : Array[Array[Double]] = {
+            implicit val codec = Codec("UTF-8")
+            codec.onMalformedInput(CodingErrorAction.REPLACE)
+            codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+            val bufferedSource = io.Source.fromFile(file)
+            var cost_matrix: Array[Array[Double]] = null
+            var j: Int = 0
+            var k: Int = 0
+            var first_line: Boolean = true
+            for (line <- bufferedSource.getLines()) {
+                if(!first_line) {
+                    var cols = line.split(delimiter).map(_.trim)
+                    // skips first columns
+                    cols = cols.slice(1, cols.size)
+
+                    // memory optimization
+                    if(cost_matrix == null) {
+                        cost_matrix = Array.ofDim[Double](cols.size, cols.size)
+                    }
+
+                    for (cell <- cols) {
+                        cost_matrix(j)(k) = cell.toDouble
+                        k += 1
+                    }
+                    k = 0
+                    j += 1
+                }
+                else {
+                    // skips header
+                    first_line = false
+                }
+            }
+            return(cost_matrix)
+        }
+    }
+
+    object TXTReader {
+        def read(file: String) : Array[Array[Double]] = {
+            val sc = new Scanner(new File(file))
+            val dimension = sc.nextInt()
+            var cost_matrix = Array.ofDim[Double](dimension, dimension)
+
+            for(i <- 0 to dimension-1) {
+                for(j <- 0 to dimension-1) {
+                    cost_matrix(i)(j) = sc.nextDouble()
+                }
+            }
+            return(cost_matrix)
+        }
+    }
+}
